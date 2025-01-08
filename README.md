3047dadd2e35605be169301f494673da9710c042
Charles <charlesnwoye2@gmail.com>
2025-01-08 16:54:48 +0100

    Implemented Code to fetch weather data

---
+++ README.md
@@ -0,0 +1,87 @@
+# 30 Days DevOps Challenge - Weather Dashboard
+Day 1: Building a weather data collection system using Azure Blob storage and OpenWeather API
+
+# Weather Data Collection System - DevOps Day 1 Challenge
+
+## Project Overview
+WeatherDashboard is a Python application that fetches weather data for specified cities using the OpenWeather API and stores the data in Azure Blob Storage for further analysis or use. This project demonstrates core DevOps principles by combining:
+
+- External API Integration (OpenWeather API)
+- Cloud Storage (Azure Blob storage)
+- Infrastructure as Code
+- Version Control (Git)
+- Python Development
+- Error Handling
+- Environment Management
+
+## Features
+- Fetches real-time weather data from the OpenWeather API
+- Stores weather data in Azure Blob Storage in JSON format
+- Automatically creates the necessary Azure Blob container if it doesn't exist
+- Includes timestamped data for tracking changes over time
+- Supports multiple cities tracking
+
+## Prerequisites
+- Python 3.x
+- Azure Storage Account credentials
+- OpenWeather API key
+
+## Dependencies
+- python-dotenv
+- requests
+- azure-storage-blob
+
+## Project Striucture
+```shell
+01_weather_dashboard/g
+├── src/
+│   ├── __init__.py
+│   └── weather_dashboard.py
+├── tests/
+├── data/
+├── .gitignore
+├── .env
+├── requirements.txt
+└── README.md
+```
+
+## Running the application
+1. Clone the repository
+    ```shell
+    git clone https://github.com/Jekwulum/01_weather_dashboard.git
+    cd 01_weather_dashboard
+    ```
+2. Create a virtual environment:
+   ```shell
+   python -m venv .venv
+   source .venv/Scripts/activate  # On Windows use `.venv\Scripts\activate`
+   ```
+3. Install the dependencies:
+   ```shell
+   pip install -r requirements.txt
+   ```
+4. Configure environment variables (.env):
+   ```shell
+   OPENWEATHER_API_KEY=your_api_key
+   STORAGE_ACCOUNT_URL=your_storage_account_url
+   STORAGE_ACCOUNT_KEY=your_storage_account_key
+   ```
+5. Run the application:
+   ```shell
+   python src/weather_dashboard.py
+   ```
+
+## What I learned
+Azure Blob storage container creation and management
+Environment variable management for secure API keys
+Python best practices for API integration
+Git workflow for project development
+Error handling in distributed systems
+Cloud resource management
+
+## Future Enhancements
+Add weather forecasting
+Implement data visualization
+Add more cities
+Create automated testing
+Set up CI/CD pipeline
---
+++ src/weather_dashboard.py
@@ -0,0 +1,92 @@
+import os
+import json
+import requests
+from azure.storage.blob import BlobServiceClient
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class WeatherDashboard:
+  def __init__(self):
+    """Initialize the WeatherDashboard with necessary configurations and Blob Storage setup."""
+    self.api_key = os.getenv("OPENWEATHER_API_KEY")
+    self.storage_account_url = os.getenv("STORAGE_ACCOUNT_URL")
+    self.storage_account_key = os.getenv("STORAGE_ACCOUNT_KEY")
+    
+    # self.container_name = "weather-data"
+    # self.blob_service_client = BlobServiceClient(account_url=self.storage_account_url, credential=self.storage_account_key)
+    # self.container_client = self._ensure_container_exists()
+  
+  def _create_container_if_not_exists(self):
+    """Ensure the Blob container exists, creating it if necessary."""
+    container_client = self.blob_service_client.get_container_client(self.container_name)
+    if not container_client.exists():
+      container_client.create_container()
+      print(f"Container '{self.container_name}' created.")
+      return container_client
+
+  def upload_to_blob(self, weather_data, city):
+    """upload data to blob"""
+    if not weather_data:
+      return False
+    
+    timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
+    file_name = f"weather-data/{city}-{timestamp}.json"
+    
+    try:
+      weather_data['timestamp'] = timestamp
+      blob_client = self.container_client.get_blob_client(file_name)
+      blob_client.upload_blob(weather_data, overwrite=True)
+      print(f"FIle '{file_name}' uploaded to container '{self.container_name}'")
+      return True
+    except Exception as e:
+      print(f"Error uploading blob for {city}: {e}")
+      return False
+
+  def fetch_weather_data(self, city):
+    """Fetch weather data from OpenWeather API"""
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+            "q": city,
+            "appid": self.api_key,
+            "units": "imperial"
+        }
+    
+    try:
+      response = requests.get(base_url, params=params)
+      response.raise_for_status()
+      return response.json()
+    except requests.exceptions.RequestException as e:
+      print(f"Error fetching weather data: {e}")
+      return None
+
+
+def main():
+  """Program entry point"""
+  dashboard = WeatherDashboard()
+
+  cities = ["Philadelphia", "Seattle", "Lagos", "London", "Bozeman"]
+
+  for city in cities:
+    print(f"\nFetching weather for {city}...")
+    weather_data = dashboard.fetch_weather_data(city)
+    if weather_data:
+      temp = weather_data['main']['temp']
+      feels_like = weather_data['main']['feels_like']
+      humidity = weather_data['main']['humidity']
+      description = weather_data['weather'][0]['description']
+
+      print(f"Temperature: {temp}°F")
+      print(f"Feels like: {feels_like}°F")
+      print(f"Humidity: {humidity}%")
+      print(f"Conditions: {description}")
+
+    #   success = dashboard.upload_to_blob(weather_data, city)
+    #   if success:
+    #     print(f"Weather data for {city} saved to {dashboard.container_name}")
+    # else:
+    #   print(f"Failed to fetch weather data for {city}")
+
+if __name__ == '__main__':
+  main()
